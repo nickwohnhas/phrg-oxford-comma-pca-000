96df6fcc3a495ec668a7cd1b24d011694a1fb3d9
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-05-10 14:33:30 -0400

    Oxford Comma Lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_element = array.slice(array.length - 1)
+    rest_of_array = array.slice(0,array.length - 1)
 
+    return rest_of_array.join(", ").insert(-1,",").insert(-1," and #{last_element}")
+  end
 end
